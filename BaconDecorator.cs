42e0658bd9dd75f1c46c2124bccbd2b5c8eb8d18
Claudio Ribeiro <claudio115789@outlook.com>
2021-01-06 23:04:00 -0300

    Create BaconDecorator.cs

---
+++ BaconDecorator.cs
@@ -0,0 +1,20 @@
+﻿using ExampleDecoratorPattern.Interfaces;
+
+namespace ExampleDecoratorPattern
+{
+    //Concrete Decorator
+    public class BaconDecorator : PizzaDecorator
+    {
+        public BaconDecorator(IPizza pizza)
+            : base(pizza)
+        {
+        }
+
+        public override string BuscarTipoPizza()
+        {
+            string pizza = base.BuscarTipoPizza();
+            pizza += "\r\n com extra de bacon";
+            return pizza;
+        }
+    }
+}
