aba64b38f3222b6cc9674b820cf4208cc7cad3b4
Claudio Ribeiro <claudio115789@outlook.com>
2021-01-06 23:06:17 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using ExampleDecoratorPattern.Interfaces;
+using System;
+
+namespace ExampleDecoratorPattern
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            IPizza pizza = new Pizza();
+            IPizza queijoDecorator = new QueijoDecorator(pizza);
+            IPizza baconDecorator = new BaconDecorator(queijoDecorator);
+            IPizza cebolaDecorator = new CebolaDecorator(baconDecorator);
+
+            Console.WriteLine(cebolaDecorator.BuscarTipoPizza());
+            Console.WriteLine();
+        }
+    }
+}
